4e96d71c544be51a8820341ab9ccc604bc5cad6e
IanLaFlair <rahmandana08@gmail.com>
2023-02-19 23:34:56 +0700

    Deploy to Goerli and Update Readme

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,10 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWallet = require('./app/node_modules/truffle-hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = 'include globe soccer album annual flag smart candy violin rose slab aerobic';
 
 module.exports = {
   /**
@@ -64,14 +63,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWallet(mnemonic, `https://goerli.infura.io/v3/3ee53aad567e4565bc1aad64ac276e0d`),
+      network_id: 5,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
